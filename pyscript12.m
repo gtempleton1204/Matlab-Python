4eccd72632480ca215f8b5bd621a07a4df3b64ba
Garrett Daniel Templeton <gtempleton1204@unm.edu>
2024-11-05 14:37:05 -0700

    Add files via upload

---
+++ pyscript12.m
@@ -0,0 +1,13 @@
+pyversion('C:/Users/nmmik/AppData/Local/Programs/Python/Python312/python.exe')
+
+mylist = py.list({'apple', 'banana', 'orange'});
+len_of_list = py.len(mylist);
+disp(len_of_list);
+
+py_math = py.importlib.import_module('math');
+result = py_math.sqrt(25);
+disp(result);
+
+py_script = py.importlib.import_module('greet');
+greeting = py_script.greet('MATLAB');
+disp(greeting);
